b4c305b987741c3aa7b5b726b5467a1bcfde92c7
dokilove <38000425+dokilove@users.noreply.github.com>
2022-01-04 15:51:01 +0900

    const and let never use var

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert("hi");
+let a = 10;
+const b = 20;
+
+let myName = "xoxocopy";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("helllo " + myName);
+
+myName = "dokilove";
+
+console.log("hello " + myName);
+
+a = 20;
