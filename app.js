a1ce4c8f3855b1ad9404ccfe586849db6d117cb4
dokilove <38000425+dokilove@users.noreply.github.com>
2022-01-05 17:46:09 +0900

    Array

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const amIFat  = null;
-let something;
-console.log(something);
-something = 1;
-console.log(something);
-something = false;
-console.log(something);
-something = amIFat;
-console.log(something);
+const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+const nonsense = [1, 2, "hellp", false, null, true, undefined, "xoxocopy"];
+
+console.log(dayOfWeek[0], nonsense);
+
+dayOfWeek.push("sun2");
+
+console.log(dayOfWeek[7]);
