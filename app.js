a15d7b781e1557fcfc5e500c34c6af87f30582af
dokilove <38000425+dokilove@users.noreply.github.com>
2022-01-13 15:33:20 +0900

    #3.3 Events

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const title = document.querySelectorAll(".hellolive h1:first-child");
+const title = document.querySelectorAll(".hellolive:first-child h1");
 const grab3 = document.querySelector("#grab3 h1");
 
-console.log(title);
-console.log(grab3);
+console.dir(title);
+title[0].style.color = "green";
+console.dir(grab3);
+grab3.style.color = "blue";
+
+function handleTiteClick(){
+    title[1].style.color = "wheat";
+}
+
+title[1].addEventListener("click", handleTiteClick);
