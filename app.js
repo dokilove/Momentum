828a2fb4d6fc3e3959108fa3613e13a8f342158c
dokilove <38000425+dokilove@users.noreply.github.com>
2022-01-14 15:34:41 +0900

    #3.7 CSS in Javascript part Two

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 const h1 = document.querySelectorAll(".hellolive:first-child h1");
 const grab3 = document.querySelector("#grab3 h1");
 
-console.dir(h1);
-h1[0].style.color = "green";
-console.dir(grab3);
-grab3.style.color = "blue";
 
 function handleTiteClick(){
-    const currentColor = h1[1].style.color;
-    let newColor;
-    if (currentColor === "blue"){
-        newColor = "tomato"
-    }else{
-        newColor = "blue";
-    }
-    h1[1].style.color = newColor;
+    const clickedClass = "clicked";
+    if (h1[1].className === clickedClass){
+        h1[1].className = "";
+    } else{
+        h1[1].className = clickedClass;
+    }    
 }
 
 h1[1].addEventListener("click", handleTiteClick);
