c075979713d36eb6f440e95ae23ea26eaa1a3122
dokilove <38000425+dokilove@users.noreply.github.com>
2022-01-14 15:23:23 +0900

    #3.6 CSS in Javascript

--- app.js
+++ app.js
@@ -7,39 +7,14 @@ console.dir(grab3);
 grab3.style.color = "blue";
 
 function handleTiteClick(){
-    h1[1].style.color = "wheat";
+    const currentColor = h1[1].style.color;
+    let newColor;
+    if (currentColor === "blue"){
+        newColor = "tomato"
+    }else{
+        newColor = "blue";
+    }
+    h1[1].style.color = newColor;
 }
 
-function handleMouseEnter(){
-    h1[1].style.color = "skyblue";
-}
-
-function handleMouseLeave(){
-    h1[1].style.color = "blue";
-}
-h1[1].onclick = handleTiteClick;
-h1[1].onmouseenter = handleMouseEnter;
-h1[1].addEventListener("mouseleave", handleMouseLeave);
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-
-function handleWindowOffline(){
-    alert("SOS no wifi");
-}
-
-function handleWindowOnline(){
-    alert("All right");
-}
-
-window.addEventListener("resize", handleWindowResize);
-
-window.addEventListener("copy", handleWindowCopy);
-
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1[1].addEventListener("click", handleTiteClick);
