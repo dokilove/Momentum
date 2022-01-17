ce642c4f7187709f1b218ca9c0d8fd66b49ea8f5
dokilove <38000425+dokilove@users.noreply.github.com>
2022-01-17 16:20:09 +0900

    #4.3 Events part Two

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const loginForm = document.querySelector("#login-form");
 
 const loginInput = document.querySelector("#login-form input");
 
+
+const link = document.querySelector("a");
+
 function onLoginSubmit(event){
     event.preventDefault();
     //const username = loginInput.value;
     console.log(loginInput.value);  
 }
 
+function handleLink(event){
+    event.preventDefault();
+    console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLink);
