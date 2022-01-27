5157e87487e58e19bdf1472c2698c8057914a7c5
dokilove <38000425+dokilove@users.noreply.github.com>
2022-01-27 15:57:11 +0900

    #8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,15 @@
+const API_KEY = "e331e44680396bbb27732049e71d6ae9";
+
 function onGeoOK(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("you live in ", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;        
+    });
 }
 
 function onGeoError(){
@@ -9,3 +17,6 @@ function onGeoError(){
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
+
+
+
